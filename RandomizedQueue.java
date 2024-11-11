665930add4d43638f3dcf53fee5a72bccc55a254
James Vaisman <lavaisman@gmail.com>
2024-11-10 22:57:29 -0800

    Update RandomizedQueue.java

--- RandomizedQueue.java
+++ RandomizedQueue.java
@@ -1,57 +1,61 @@
-import java.util.Iterator; 
-import java.util.NoSuchElementException; 
-
-import edu.princeton.cs.algs4.StdIn; 
-import edu.princeton.cs.algs4.StdOut; 
-import edu.princeton.cs.algs4.StdRandom;  // Corrected import
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom; // Corrected import
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    private Item[] items = (Item[]) new Object[2]; 
-    private int size = 0; 
+    private Item[] items = (Item[]) new Object[2];
+    private int size = 0;
 
     // construct an empty randomized queue
     public RandomizedQueue() {}
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        return size == 0; 
+        return size == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        return size; 
+        return size;
     }
 
     // add the item
     public void enqueue(Item item) {
-        if (item == null) {  // Corrected comparison operator
+        if (item == null) {
             throw new NullPointerException("Can't enqueue null item");
         }
-        this.items[size++] = item; 
+        this.items[size++] = item;
         if (size == this.items.length) {
-            resize(2 * this.items.length); 
+            resize(2 * this.items.length);
         }
-        swapItem();  // Corrected method name
+        swapItem(); // Corrected method name
     }
 
     // remove and return a random item
     public Item dequeue() {
         if (size == 0) {
-            throw new NoSuchElementException("Can't dequeue, queue is empty");  // Fixed typo in message
+            throw new NoSuchElementException("Can't dequeue, queue is empty");
         }
-        Item item = this.items[size - 1];  // Fixed index access
-        size--; 
+        int randIndex = StdRandom.uniform(size); // Pick random index for dequeue
+        Item item = this.items[randIndex];
+        this.items[randIndex] = this.items[size - 1]; // Swap random item with last item
+        this.items[size - 1] = null;
+        size--;
+        
+        // Resize array if necessary
         if (size > 0 && size == this.items.length / 4) {
-            resize(this.items.length / 2); 
+            resize(this.items.length / 2);
         }
-        this.items[size] = null; 
-        return item; 
+
+        return item;
     }
 
     // return a random item (but do not remove it)
     public Item sample() {
         if (size == 0) {
-            throw new NoSuchElementException("Can't sample, queue is empty");  // Fixed exception message
+            throw new NoSuchElementException("Can't sample, queue is empty");
         }
         int i = StdRandom.uniform(size);
         return this.items[i];
@@ -63,10 +67,10 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     private class ListIterator implements Iterator<Item> {
-        private int i = 0;  // Ensure index starts from 0
+        private int i = 0;
 
         public boolean hasNext() {
-            return i < size;  // Fixed iterator logic
+            return i < size;
         }
 
         public void remove() {
@@ -77,8 +81,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             if (!hasNext()) {
                 throw new NoSuchElementException();
             } else {
-                Item item = items[i++];
-                return item;
+                return items[i++];
             }
         }
     }
@@ -92,7 +95,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     private void swapItem() {
-        if (size > 0) {  // Added safety check
+        if (size > 0) {
             int i = StdRandom.uniform(size);
             Item temp = items[i];
             items[i] = items[size - 1];
@@ -109,6 +112,5 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             else
                 rq.enqueue(s);
         }
-    } // unit testing (optional)
+    }
 }
-
